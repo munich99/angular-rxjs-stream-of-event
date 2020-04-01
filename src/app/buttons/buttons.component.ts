589cd2cd53d8b1b7dc1713edbf9e7f525223efa4
munich99 <sebastian.sawicz@bsb-muenchen.de>
2020-04-01 14:25:35 +0200

    combine 3 separate callbacks/streams of values together

--- src/app/buttons/buttons.component.ts
+++ src/app/buttons/buttons.component.ts
@@ -10,21 +10,29 @@ export class ButtonsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    document.addEventListener('click', evt => {
-     //  alert('I am tiggert by Eventllistener'); 
+
+    
+
+    document.addEventListener('click', evt => { 
+
       console.log(evt, 'I am tiggert by Eventllistener');
+
+      setTimeout(() => {
+        let counter = 1;
+          console.log('I am 3rd Value Stream and i am starting 2rd Stream of Value');
+          setInterval(() => {
+            counter++;
+            console.log(counter);
+          }, 1000);
+      }, 3000 ); 
+
     });
 
-    let counter = 1;
+    
 
-    setInterval(() => {
-      counter++;
-      console.log(counter);
-    }, 1000);
 
-    setTimeout(() => {
-      console.log('I am 3rd Value Stream')
-  }, 3000 )
+
+
 
 
 
